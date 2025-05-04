e112a54d904bf78c8a7c51f1b93b07b38e3ff3b9
samk <202000734@est.umss.edu>
2025-05-04 19:11:36 -0400

    config vercel json second

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+import app from '../src/index';
+import { createServer } from 'http';
+
+module.exports = (req: any, res: any) => {
+    const server = createServer(app);
+    server.emit('request', req, res);
+};
